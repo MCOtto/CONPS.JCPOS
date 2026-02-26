3558dd70579bd908ccb5e08f3facb01a9b12fd7e
Mark Otto <motto@Rufa.local>
2026-02-25 19:41:44 -0700

    Added grass and forb descriptions to create a factor variable Made a grid of new variables to get predictions of the soil and species combinations.

--- R/Germination.R
+++ R/Germination.R
@@ -1,9 +1,19 @@
 library(xlsx)
 library(here)
 library(dplyr)
+library(tidyr)
 library(stringr)
+library(IPMbook)
 # Read the germination data
 csoil.type <- c("Peat", "Mineral/Compost")
+
+# Grass BOGR Blue gramma
+# Grass HECO Hesperostipa comata
+# Forb HEVI Helleborus viridis
+# Grass? NAVI ?
+# Grass PASM Paspalum genus n the Poaceae family
+# Forb  SYER White heath aster (Symphyotrichum ericoides)
+cfrb.grs <- c("Forb", "Forb", "Grass", "Grass", "Forb", "Grass", "Grass", "Forb")
 cspecies <- c("ACMI", "ARLU", "BOGR", "HECO", "HEVI", "NAVI", "PASM", "SYER")
 Germination <- read.xlsx(
   file = paste0(here("ori"), "/JCOS data - MG.xlsx"),
@@ -20,11 +30,9 @@ Germination <- read.xlsx(
     )
   )
 
-GrmnMdlM <- glm(
-  cbind(count, 98 - count) ~ species + soil.type,
-  data = Germination,
-  binomial(link = "logit")
-)
+GrmnMdlM <- glm(cbind(count, 98 - count) ~ species + soil.type,
+                data = Germination,
+                binomial(link = "logit"))
 summary(GrmnMdlM)
 
 
@@ -39,6 +47,26 @@ anova(GrmnMdlI)
 
 AIC(GrmnMdlM, GrmnMdlI)
 
+
 # Make an expanded grid of species and soil types
-expan
-pred <- predict(model, newdata, type="link", se.fit=TRUE)
+CmbFac <- expand_grid(species = cspecies, soil.type = csoil.type) %>%
+  mutate(
+    soil.type = factor(soil.type, csoil.type),
+    species = factor(species, cspecies)
+  )
+
+pred <- predict(
+  object = GrmnMdlM,
+  newdata = CmbFac,
+  type = "response",
+  se.fit = TRUE
+)
+
+Pred <- bind_cols(CmbFac,Mean = pred$fit, SE = pred$se.fit) %>%
+  mutate(
+    Median = qbeta2(0.5, mean = Mean, sd = SE),
+    CI05 = qbeta2(0.05, mean = Mean, sd = SE),
+    CI95 = qbeta2(0.95, mean = Mean, sd = SE)
+  )
+
+
