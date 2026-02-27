088e4a57e83bc1eafabe7831e38052eb5411fb9a
Mark Otto <motto@Rufa.local>
2026-02-27 16:44:28 -0700

    Removed SYER from the species because BOGR grew the its plots. Added the fertilized factor. Found the model with the interactions was best but the data is too scattered to support all the estimates.  Ask Maggie how to cut it down. Made code to calculate the proportions and CIs

--- R/Survival.R
+++ R/Survival.R
@@ -1,11 +1,16 @@
 library(xlsx)
 library(here)
 library(dplyr)
+library(tidyr)
 library(stringr)
+library(IPMbook)
+library(ggplot2)
+
 # Read the Survival data
 csoil.type <- c("MC", "peat", "S3")
+cfertilized <-c("NF", "F")
 cherb.type <- c("Forb", "Grass")
-cspecies <- c("ACMI", "SYER", "ARLU", "HEVI", "PASM", "BOGR", "NAVI", "HECO")
+cspecies <- c("ACMI", "ARLU", "HEVI", "PASM", "BOGR", "NAVI", "HECO")
 # , "SYER" missing?
 cplantdate <- c("2025-07-10", "2025-08-14", "2025-09-11")
 cCol <- c(
@@ -19,6 +24,7 @@ cCol <- c(
   "survival.rate"
 )
 
+# Read the survival data from the simplified sheet.
 Survival <- read.xlsx(
   file = paste0(here("ori"), "/JCOS data - MG.xlsx"),
   sheetName = "alldatasimplified",
@@ -39,31 +45,123 @@ Survival <- read.xlsx(
     soil.type = factor(soil.type, levels = csoil.type),
     species = factor(species, levels = cspecies),
     plant.date = factor(plant.date, levels = cplantdate),
+    fertilized = factor(fertilized, levels = cfertilized),
     survived = as.integer(survived)
   ) %>%
   select(all_of(cCol))
 
+# Model selection
 SrvlMdlM <- glm(
   cbind(survived, planted - survived) ~ species + soil.type + plant.date + fertilized,
   family = binomial(link = "logit"),
-  data = Survival,
-  subset = planted > 0
+  data = Survival
+  # ,subset = planted > 0
 )
 summary(SrvlMdlM)
 anova(SrvlMdlM)
 
-
 SrvlMdlI <- glm(
-  cbind(count, 98 - count) ~ species + soil.type + species *
-    soil.type,
+  cbind(survived, planted - survived) ~ species * soil.type + species * plant.date + fertilized,
   data = Survival,
-  binomial(link = "logit")
+  family = binomial(link = "logit")
 )
 summary(SrvlMdlI)
 anova(SrvlMdlI)
 
-AIC(SrvlMdlM, SrvlMdlI)
+# Negative Hessian
+SrvlMdlLME <- glmmTMB(
+  cbind(survived, planted - survived) ~ 1 + (species|soil.type) + plant.date + fertilized,
+  # (species|plant.date)
+  data = Survival,
+  family = binomial(link = "logit")
+)
+summary(SrvlMdlLME)
+anova(SrvlMdlLME)
+
+SrvlMdlnF <- glm(
+  cbind(survived, planted - survived) ~ species * soil.type + species * plant.date,
+  data = Survival,
+  family = binomial(link = "logit")
+)
+summary(SrvlMdlnF)
+anova(SrvlMdlnF)
+
+SrvlMdlF <- glm(
+  cbind(survived, planted - survived) ~ species * soil.type * plant.date + fertilized,
+  data = Survival,
+  binomial(link = "logit")
+)
+summary(SrvlMdlF)
+anova(SrvlMdlF)
+
+# The interaction with fertilized is best.
+AIC(SrvlMdlM, SrvlMdlI, SrvlMdlnF, SrvlMdlF)
 
 # Make an expanded grid of species and soil types
-expan
-pred <- predict(model, newdata, type="link", se.fit=TRUE)
+
+# Make an expanded grid of species, soil types, and plant dates
+CmbFac <- expand_grid(
+  species = cspecies,
+  soil.type = csoil.type,
+  plant.date = cplantdate,
+  fertilized = cfertilized
+) %>%
+  mutate(
+    soil.type = factor(soil.type, csoil.type),
+    plant.date = factor(plant.date, cplantdate),
+    fertilized = factor(fertilized, levels = cfertilized),
+    herb.type = factor(ifelse(
+      str_sub(species, 1, 4) %in% c("ACMI", "SYER", "ARLU", "HEVI"),
+      "Forb",
+      "Grass"
+    ), levels = cherb.type),
+    species = factor(species, cspecies)
+  )
+
+# can work these to get the herb type and soil type statistics and plots.
+fitterms <- predict(
+  object = SrvlMdlI,
+  newdata = CmbFac,
+  type = "terms",
+  se.fit = TRUE
+)
+
+intercept <- attr(fitterms$fit, "constant")
+tfit <- fitterms$fit
+colnames(tfit) <- paste(colnames(tfit), "Mn", sep = "")
+tse <- fitterms$se.fit
+colnames(tse) <- paste(colnames(tse), "SE", sep = "")
+
+Fit <- bind_cols(CmbFac, tfit, tse)
+
+# Make the marginal matrices
+# Species
+# Note that must be < sqrt(p * (1 - p))
+# HEVI and HECO are sparse.
+
+SpeciesFit <- Fit %>%
+  distinct(species, herb.type, speciesMn, speciesSE) %>%
+  mutate(
+    lgt.species = intercept + speciesMn,
+    lgt.speciesSE = speciesSE,
+    pspecies = 1 / (1 + exp(-lgt.species)),
+    MxSE = 0.8 * sqrt(pspecies * (1 - pspecies)),
+    pspeciesSE = pmin(MxSE, pspecies * (1 - pspecies) * lgt.speciesSE),
+    Median = qbeta2(0.5, mean = pspecies, sd = pspeciesSE),
+    CI05 = qbeta2(0.05, mean = pspecies, sd = pspeciesSE),
+    CI95 = qbeta2(0.95, mean = pspecies, sd = pspeciesSE)
+  ) %>%
+  select(
+    species,
+    herb.type,
+    lgt.species,
+    lgt.speciesSE,
+    pspecies,
+    pspeciesSE,
+    Median,
+    CI05,
+    CI95
+  )
+
+
+
